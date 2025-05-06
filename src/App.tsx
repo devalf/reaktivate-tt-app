9bb07236651a85f0d51cc7e70922e19e5fe3a8a5
Oleksiy Petrychko <mykolasich@gmail.com>
2025-05-06 13:52:39 +0300

    Basic setup

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import './App.css';
+
+export const App: FC = () => {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea esse exercitationem minima?
+        Deleniti ea earum eius esse laboriosam maiores nam natus officia, omnis perferendis,
+        praesentium saepe temporibus veritatis? Asperiores dolore doloribus excepturi iusto laborum
+        tenetur voluptatum? Accusamus architecto deleniti ea, esse harum incidunt ipsam quia quis
+        voluptatum. A accusantium aliquid aspernatur assumenda, beatae blanditiis dignissimos
+        distinctio dolore eaque eligendi error facere fuga, impedit ipsa ipsum iure laudantium minus
+        non numquam obcaecati omnis perspiciatis praesentium provident quaerat quasi qui quis quod
+        reiciendis reprehenderit rerum sed, totam unde veniam? Aut delectus doloremque dolores
+        eveniet excepturi ipsa, natus nemo quam similique. Accusantium aut, culpa cum facere non
+        porro tenetur vitae? Ab, alias aut corporis culpa fugit impedit nisi quam voluptas! A atque,
+        consequatur cum deserunt dolorum, eligendi eum id, itaque nobis non optio placeat possimus
+        quaerat quasi quibusdam quo quos repellendus vero! Ipsa, ratione, saepe. Ab atque beatae
+        commodi dicta, earum eligendi excepturi explicabo fugiat fugit, harum molestias mollitia
+        possimus quaerat quasi quidem ratione repellendus, rerum similique voluptatem voluptatibus?
+        Aut debitis ex natus qui. Aspernatur eum facilis pariatur tenetur vel. Ad beatae blanditiis
+        delectus dolores in iste laborum magni, minima officiis quibusdam, quod recusandae
+        repellendus repudiandae sapiente temporibus tenetur veniam voluptatum. Consequuntur,
+        pariatur!
+      </div>
+    </>
+  );
+};
